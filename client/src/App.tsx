1895a1047a7baaf5e34791d08bb92217b9e43212
laxman-goud <codewithlaxman92@gmail.com>
2025-12-23 07:28:45 +0530

    client(chore): add basic set up for app and main files

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+import Sidebar from "./components/Sidebar"
+import ChatBox from "./components/ChatBox"
+import Credits from "./pages/Credits"
+import Community from "./pages/Community"
+
+
+const App = () => {
+  return (
+      <>
+        <div className="dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white">
+          <div className="flex h-screen w-screen">
+            <Sidebar />
+            <Routes>
+              <Route path="/" element={<ChatBox />} />
+              <Route path="/credits" element={<Credits />} />
+              <Route path="/community" element={<Community />} />
+            </Routes>
+          </div>
+        </div>
+      </>
+  )
+}
+
+export default App
