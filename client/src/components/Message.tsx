0734cf212339d4301ce798459556742217236451
laxman-goud <codewithlaxman92@gmail.com>
2025-12-24 20:41:24 +0530

    client(feature): implement Message component with timestamp and markdown support

---
+++ client/src/components/Message.tsx
@@ -0,0 +1,56 @@
+import { useEffect } from "react";
+import moment from "moment";
+import Markdown from "react-markdown";
+import Prism from "prismjs";
+
+import { assets } from "../assets/assets";
+import type { IMessage } from "../types/chat.types";
+
+interface MessageProps {
+    message: IMessage;
+}
+
+const Message = ({ message }: MessageProps) => {
+    useEffect(() => {
+        Prism.highlightAll();
+    }, [message.content]);
+
+    return (
+        <div>
+            {message.role === "user" ? (
+                <div className="flex items-start justify-end my-4 gap-2">
+                    <div className="flex flex-col gap-2 p-2 px-4 bg-slate-50 dark:bg-[#57317C]/30 border border-[#80609F]/30 rounded-md max-w-2xl">
+                        <p className="text-sm dark:text-primary">{message.content}</p>
+                        <span className="text-xs text-gray-400 dark:text-[#B1ACC0]">
+                            {moment(message.timestamp).fromNow()}
+                        </span>
+                    </div>
+                    <img
+                        className="w-8 rounded-full"
+                        src={assets.user_icon}
+                        alt="icon"
+                    />
+                </div>
+            ) : (
+                <div className="inline-flex flex-col gap-2 p-2 px-4 max-w-2xl bg-primary/20 dark:bg-[#57317C]/30 border border-[#80609F]/30 rounded-md my-4">
+                    {message.isImage ? (
+                        <img
+                            className="w-full max-w-md mt-2 rounded-md"
+                            src={message.content}
+                            alt="generated"
+                        />
+                    ) : (
+                        <div className="text-xs dark:text-primary reset-tw">
+                            <Markdown>{message.content}</Markdown>
+                        </div>
+                    )}
+                    <span className="text-xs text-gray-400 dark:text-[#B1A6C0]">
+                        {moment(message.timestamp).fromNow()}
+                    </span>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Message;
