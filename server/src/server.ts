7c3f92e75e153d64f83a540d13ab3a906aedab90
laxman-goud <codewithlaxman92@gmail.com>
2025-12-28 11:42:32 +0530

    server(feat): add chat, message routers to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import "dotenv/config";
 
 import connectDB from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
+import chatRouter from "./routes/chatRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
-dotenv.config();
+// dotenv.config();
 
 const app = express();
 const PORT = Number(process.env.PORT) || 5000;
@@ -23,6 +25,8 @@ app.get("/", (_req: Request, res: Response) => {
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/chat", chatRouter);
+app.use("/api/message", messageRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
