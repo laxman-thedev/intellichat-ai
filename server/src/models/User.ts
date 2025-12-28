180ab25898a9a3c86880228a984793a9506a848f
laxman-goud <codewithlaxman92@gmail.com>
2025-12-28 08:07:04 +0530

    server(feat): add user model

---
+++ server/src/models/User.ts
@@ -0,0 +1,69 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+import bcrypt from "bcryptjs";
+
+/* ------------------ TYPES ------------------ */
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    credits: number;
+    comparePassword(password: string): Promise<boolean>;
+}
+
+/* ------------------ SCHEMA ------------------ */
+
+const userSchema = new Schema<IUser>(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+        credits: {
+            type: Number,
+            default: 20,
+        },
+    },
+    { timestamps: true }
+);
+
+/* ------------------ MIDDLEWARE ------------------ */
+
+// Hash password before saving
+userSchema.pre<IUser>("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+/* ------------------ METHODS ------------------ */
+
+userSchema.methods.comparePassword = async function (
+    this: IUser,
+    enteredPassword: string
+): Promise<boolean> {
+    return bcrypt.compare(enteredPassword, this.password);
+};
+
+/* ------------------ MODEL ------------------ */
+
+const User: Model<IUser> =
+    mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+
+export default User;
