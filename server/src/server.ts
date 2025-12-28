97366a07b33ab37fdc86a450bbe09a5216c1ad7d
laxman-goud <codewithlaxman92@gmail.com>
2025-12-28 17:13:07 +0530

    server(feat): add stripe routes to app

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,8 @@ import connectDB from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
 import chatRouter from "./routes/chatRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
+import creditRouter from "./routes/creditRoutes.js";
+import { stripeWebhooks } from "./controllers/webhooks.js";
 
 // dotenv.config();
 
@@ -14,6 +16,9 @@ const PORT = Number(process.env.PORT) || 5000;
 
 // Connect DB
 await connectDB();
+
+// stripe Webhooks
+app.post("/api/stripe", express.raw({type: "application/json"}), stripeWebhooks)
 
 // Middlewares
 app.use(cors());
@@ -27,7 +32,10 @@ app.get("/", (_req: Request, res: Response) => {
 app.use("/api/user", userRouter);
 app.use("/api/chat", chatRouter);
 app.use("/api/message", messageRouter);
+app.use("/api/credit", creditRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+export default app;
